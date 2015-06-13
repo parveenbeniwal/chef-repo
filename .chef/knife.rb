14d182a31fbc389a0b05fc1a9c3a2462e05bda7f
Parveen Kumar <parveenkumar@HC14-04.local>
2015-06-14 01:17:32 +0530

    Added chef_demo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,3 +9,6 @@ validation_client_name   "uniquecons-validator"
 validation_key           "#{current_dir}/uniquecons-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/uniquecons"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright "Unique Consultants"
+cookbook_license "apachev2"
+cookbook_email "parveen.beniwal@gmail.com"
